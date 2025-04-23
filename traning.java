e22e3d7b5be1228732d13fa8412de3f804815270
Rudranshweb <rudranshsirohia@gmail.com>
2025-04-23 11:39:17 +0530

    Add files via upload

--- traning.java
+++ traning.java
@@ -2,24 +2,16 @@ import java.util.Scanner;
 public class traning {
     public static void main(String[] args) {
         String str="hello";
-        String newstr="";
-        for(int i=0;i<str.length();i++){
-            char c=str.charAt(i);
-            if (newstr.indexOf(c)==-1){
-                newstr+=c;
+        char[] ch=str.toCharArray();
+        for(int i=0;i<ch.length;i++){
+            for(int j=i+1;j<ch.length;j++){
+                    if(ch[i]==ch[j]){
+                        System.out.println(ch[i]);
+                        return;
+                    }
+                
             }
-            
         }
-
-    System.out.println(str);
-    System.out.println(newstr);
-    
-    
-    
-    
-    
-    
-    
+        //System.out.println(i);
     }
-
 }
