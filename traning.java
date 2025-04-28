b2e0b8070083e5c7f9d14cf9d320d2f1a0014321
Rudranshweb <rudranshsirohia@gmail.com>
2025-04-28 15:55:39 +0530

    Add files via upload

--- traning.java
+++ traning.java
@@ -1,15 +1,16 @@
 import java.util.Scanner;
 public class traning {
-  //print number 1 to n
-    static void element(int n){
-      for(int i=1;i<=n;i++){
+  //print even number a to n
+    static void element(int a, int n){
+      for(int i=a;i<=n;i=i+2){
         System.out.println(i);
       }
     }
       public static void main(String[] args) {
       Scanner sc= new Scanner(System.in);
-      int n =sc.nextInt();  
-      element(n);
+      int a =sc.nextInt();  
+      int n =sc.nextInt();
+      element(a,n);
   
     
     
