3b3a5c4abb462867762a605214aed7f890f2d9bd
Rudranshweb <rudranshsirohia@gmail.com>
2025-04-28 16:00:39 +0530

    Add files via upload

--- traning.java
+++ traning.java
@@ -1,16 +1,16 @@
 import java.util.Scanner;
 public class traning {
-  //print even number a to n
-    static void element(int a, int n){
-      for(int i=a;i<=n;i=i+2){
+  //print natural number n in reverse 
+    static void element(int n){
+      for(int i=n;i>=0;i--){
         System.out.println(i);
       }
     }
       public static void main(String[] args) {
       Scanner sc= new Scanner(System.in);
-      int a =sc.nextInt();  
+      //int a =sc.nextInt();  
       int n =sc.nextInt();
-      element(a,n);
+      element(n);
   
     
     
