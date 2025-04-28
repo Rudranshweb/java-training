63264ec606191a23f5a7b2c636638c925909db36
Rudranshweb <rudranshsirohia@gmail.com>
2025-04-28 15:23:50 +0530

    Add files via upload

--- traning.java
+++ traning.java
@@ -1,20 +1,36 @@
 import java.util.Scanner;
 public class traning {
-  static int targetsum(int[] arr){
-        int target=8;
-        for(int i=0;i<arr.length;i++){
-          if(arr[i]==target){
-            System.out.println(i);
-          }
-        }
-        return -1;
-  }
-      public static void main(String[] args) {
-      int [] arr={5,8,6,3,6,8};
-      targetsum(arr);
-    
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        
+        for (int i = 0; i < n - 1; i++) {
 
-    
-    
+            for (int j = 1; j < n - i; j++) {
+                if (arr[j - 1] > arr[j]) {
+                    int temp = arr[j - 1];
+                    arr[j - 1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
     }
-  }
+
+    public static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+
+    // Main method to test the bubble sort
+    public static void main(String[] args) {
+        int[] arr = {64, 34, 25, 12, 22, 11, 90};
+
+        bubbleSort(arr);
+
+        System.out.println("Sorted array:");
+        printArray(arr);
+    }
+}
+
+
