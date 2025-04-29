425d0136cb2bf07732c45f5b02326ede9da11e33
Rudranshweb <rudranshsirohia@gmail.com>
2025-04-29 12:33:59 +0530

    Add files via upload

--- traning.java
+++ traning.java
@@ -1,17 +1,27 @@
 import java.util.Scanner;
 public class traning {
-  //print natural number n in reverse 
-    static void element(int n){
-      for(int i=n;i>=0;i--){
-        System.out.println(i);
+    static void bsort(String [] str){
+      //int sum=0;
+      for(int i=0;i<str.length-1;i++){
+        for(int j=0;j<str.length-i-1;j++){
+            if((int)str[j].charAt(0)>(int)str[j+1].charAt(0)){
+                String temp=str[j];
+                str[j]=str[j+1];
+                str[j+1]=temp;
+            }
+        }
       }
     }
       public static void main(String[] args) {
-      Scanner sc= new Scanner(System.in);
+      //Scanner sc= new Scanner(System.in);
       //int a =sc.nextInt();  
-      int n =sc.nextInt();
-      element(n);
-  
+      //int n =sc.nextInt();
+      String [] str={"B","A","D","C"};
+      bsort(str);
+      for(String s: str){
+        System.out.print(s +" ");
+      }
+      
     
     
     }
