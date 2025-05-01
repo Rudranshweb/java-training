4e195a94864d045ed870a0d3e4b3d9024c4b6bcb
Rudranshweb <rudranshsirohia@gmail.com>
2025-05-01 10:49:33 +0530

    Add files via upload

---
+++ training.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class training {
+    String firstname= "rahul";
+    int age=19;
+    training(String s,int a){
+        firstname=s;
+        age=a;
+        System.out.println(firstname+ " "+age);
+    }
+  }
+    class hello{
+      public static void main(String[] args) {
+        training obj=new training("rohit", 19);
+        training obj1=new training("rahul", 17);
+
+      }
+        
+        
+    }
+    
+      
+  
