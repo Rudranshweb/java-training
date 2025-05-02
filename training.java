20cf43fd8f735d5ff7bdbedadf1933a43578a0e9
Rudranshweb <rudranshsirohia@gmail.com>
2025-05-02 11:22:01 +0530

    Add files via upload

--- training.java
+++ training.java
@@ -1,26 +1,27 @@
-// Hierarchical inheritence
-class animal{
-  void eat(){
-    System.out.println("eatinggggggggggggggg");
+// Q8 
+class manager{
+  void list(){
+    System.out.println("the data of the employyy");
   }
 }
-class dog extends animal{
-  void bark(){
-    System.out.println("barkkkkkkkkkkkkkkkkkk");
+class employee extends manager{
+  void data(){
+    System.out.println("here the employyysss working");
+   // System.out.println("here the employ 2");
   }
   
 }
-class species extends animal{
-  void lazy(){
-    System.out.println("the dog is lazy");
-  }
-}
+// class species extends animal{
+//   void lazy(){
+//     System.out.println("the dog is lazy");
+//   }
+// }
 public class training{
   public static void main(String[] args) {
-    dog obj= new dog();
-    obj.eat();
-    obj.bark();
-    species obj1= new species();
-    obj1.lazy();
+    employee obj= new employee();
+    obj.list();
+    obj.data();
+    // species obj1= new species();
+    // obj1.lazy();
   }
 }
