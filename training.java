03bf81d1252596ffb5462f7e4d9d0efa93edea36
Rudranshweb <rudranshsirohia@gmail.com>
2025-05-20 14:50:49 +0530

    Add files via upload

--- training.java
+++ training.java
@@ -1,27 +1,14 @@
-// Q8 
-class manager{
-  void list(){
-    System.out.println("the data of the employyy");
-  }
-}
-class employee extends manager{
-  void data(){
-    System.out.println("here the employyysss working");
-   // System.out.println("here the employ 2");
-  }
-  
-}
-// class species extends animal{
-//   void lazy(){
-//     System.out.println("the dog is lazy");
-//   }
-// }
-public class training{
+public class training {
+
   public static void main(String[] args) {
-    employee obj= new employee();
-    obj.list();
-    obj.data();
-    // species obj1= new species();
-    // obj1.lazy();
-  }
+    int[] arr={1,2,3,4,5};
+    int lastvle =arr[arr.length-1];
+    for(int i=arr.length-1;i>0;i--){
+      arr[i]=arr[i-1];
+    }
+    arr[0]=lastvle;
+    for (int k : arr) {
+      System.out.println(k);
+    }
+}
 }
