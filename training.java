d87d8ab5a4c70944fbc8300f7397cb323d3d1eb5
Rudranshweb <rudranshsirohia@gmail.com>
2025-05-22 12:38:47 +0530

    Add files via upload

--- training.java
+++ training.java
@@ -1,40 +1,16 @@
-// public class training {
-//   class A{
-// void sum(){
-//   System.out.println("mai hu A");
-// }
-//   }
-
-// }
-// class B{
-//   void sum(){
-//     System.out.println("mai hu B");
-//   }
-// }
-// class c extends training.A,trainingB{
-//   public static void main(String[] args) {
-//     obj c=new c();
-//     obj.sum();
-//   }
-// }
-//multiple inheritence using interface
-interface client {
-  void show();
-  
-}
-interface client2{
-void show();
-  
-}
-class dev implements client,client2 {
-  public void show(){
-    System.out.println("show the common feature for client ,client2");
-  }
+public class training{
+  //try and catch method
   public static void main(String[] args) {
-    dev obj=new dev();
-    obj.show();
-  }
-}
+    int a=10;int b=0;
+    try {
+      int c=a/b;
+      System.out.println(c);
+    } catch (Exception e) {
+      System.out.println("we cannot divide by zero");
+    }
+    System.out.println("helloo");
+    System.out.println("hiii");
+}}
   
 
   
