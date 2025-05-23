ad279a72c3879abd62c6eabca5641d1620f86747
Rudranshweb <rudranshsirohia@gmail.com>
2025-05-23 13:14:40 +0530

    Add files via upload

--- training.java
+++ training.java
@@ -1,20 +1,39 @@
-import java.util.Scanner;
+// import java.util.Scanner;
 
-public class training{
-  //exception handling according to user input
-  public static void main(String[] args) {
-    String str;
-    try {
-      str=null;
-      System.out.println(str.toUpperCase());
+// public class training{
+//   //exception handling according to user input
+//   public static void main(String[] args) {
+//     Scanner sc = new Scanner(System.in);
+//     String str;
+//       try {
+//         String c=null;
+//         str=sc.nextLine();
+//      System.out.println(str.toUpperCase());
      
-    } catch (Exception e) {
-      System.out.println(e);
-    }
-    System.out.println("helloo");
-    System.out.println("hiii");
-}}
-  
+//     } catch (Exception e) {
+//       System.out.println(e);
+//     }
+//     System.out.println("hello");
+//     System.out.println("hiiiiiii");
+//       }
+      
+//     }
+import java.util.ArrayList;
+public class training {
+
+  public static void main(String[] args) {
+    ArrayList<String> arr= new ArrayList<>();
+    arr.add("rahul");
+    arr.add("harsh");
+    arr.add("karan");
+    arr.add(1,"india");
+    arr.set(3, "uk");
+    arr.get(1);
+    System.out.println(arr);
+  }
+}
+
+
 
   
 
