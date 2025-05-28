ed1c4a5e9c89d8b19b14189f3e4bd76975bc7ec2
Rudranshweb <rudranshsirohia@gmail.com>
2025-05-28 23:15:19 +0530

    Add files via upload

--- training.java
+++ training.java
@@ -1,40 +1,16 @@
-// import java.util.Scanner;
+import java.util.HashMap;
+       public class training{
+        
+        public static void main(String[] args) {
+            HashMap<String,Integer> map= new HashMap<>();
+           map.put("id0",1);
+            map.put("id1",2); 
+            map.put("id1",3);
+            System.out.println(map);
+            System.out.println(map.get("id1"));
 
-// public class training{
-//   //exception handling according to user input
-//   public static void main(String[] args) {
-//     Scanner sc = new Scanner(System.in);
-//     String str;
-//       try {
-//         String c=null;
-//         str=sc.nextLine();
-//      System.out.println(str.toUpperCase());
-     
-//     } catch (Exception e) {
-//       System.out.println(e);
-//     }
-//     System.out.println("hello");
-//     System.out.println("hiiiiiii");
-//       }
-      
-//     }
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.LinkedList;
-public class training {
-
-  public static void main(String[] args) {
-    // int [] arr={-4,7,5,-8,6,2};
-    HashSet<String> arr= new HashSet<>();
-    arr.add("CA");
-    arr.add("Ba");
-    arr.add("Ar");
-  
-  System.out.println(arr);
-    
-  }
-}
+        }
+       }
 
 
 
