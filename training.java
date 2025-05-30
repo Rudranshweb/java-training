2b5484fb52d1864594c13cf8d3ae41812027377a
Rudranshweb <rudranshsirohia@gmail.com>
2025-05-30 10:41:28 +0530

    Add files via upload

--- training.java
+++ training.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.ListIterator;
        public class training{
         public static void main(String[] args) {
             ArrayList<String> list= new ArrayList<>();
@@ -9,21 +10,26 @@ import java.util.Iterator;
                 list.add("Anil");
                 list.add("Gaurav");
                 list.add("Amit");
-                list.add("Rahul");
+                list.add("ahul");
                 list.add("Adarsh");
-                Iterator<String> it =list.iterator();
+                ListIterator<String> it =list.listIterator();
                 while(it.hasNext()){
                     String elem =it.next();
-                    System.out.println(elem);
-                    if(elem.startsWith("A")){
-                        it.remove();
-                        
+                    System.out.println(elem);}
+                    System.out.println("pree");
+                    while(it.hasPrevious()){
+                        String last=it.previous();
+                        System.out.println(last);
                     }
+                    // if(elem.startsWith("A")|| elem.startsWith("a")){
+                    //     it.remove();
+                        
+                    
                 }
-                System.out.println(list);
+                // System.out.println(list);
                 
         }
-    }
+    
 
 
 
