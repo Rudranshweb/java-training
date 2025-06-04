b0095264af6d27ef4302299b06248b3b04264f99
Rudranshweb <rudranshsirohia@gmail.com>
2025-06-04 12:49:35 +0530

    Add files via upload

--- training.java
+++ training.java
@@ -1,12 +1,20 @@
-class mythread extends Thread {
-    public void run() {
-        System.out.println("helllooooooooo");
+class myrunable implements Runnable {
+public void run() {
+    System.out.println(Thread.currentThread().getName() + "Hello");
 }
 }
 class hello{
     public static void main(String[] args) {
-        mythread t1=new mythread();
+        myrunable obj=new myrunable();
+        Thread t1= new Thread(obj);
         t1.start();
+        try{
+            t1.join();
+        }
+        catch(Exception e){
+            System.out.println("error");
+        }
         System.out.println("hhhiiii");
     }
-}
+    }
+        
